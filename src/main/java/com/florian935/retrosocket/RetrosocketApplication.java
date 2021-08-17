f558d51f56476c80ff29c78a15ebdd7a9e04834a
florian935 <florian.martin63000@gmail.com>
2021-08-18 01:57:10 +0200

    First commit

---
+++ src/main/java/com/florian935/retrosocket/RetrosocketApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.retrosocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrosocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrosocketApplication.class, args);
+	}
+
+}
