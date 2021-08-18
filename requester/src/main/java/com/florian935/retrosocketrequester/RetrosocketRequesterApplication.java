631dafeefe714c0f5124c539337315ce76779da6
florian935 <florian.martin63000@gmail.com>
2021-08-18 02:48:35 +0200

    Feat: added requester

---
+++ requester/src/main/java/com/florian935/retrosocketrequester/RetrosocketRequesterApplication.java
@@ -0,0 +1,13 @@
+package com.florian935.retrosocketrequester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrosocketRequesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrosocketRequesterApplication.class, args);
+	}
+
+}
---
+++ requester/src/test/java/com/florian935/retrosocketrequester/RetrosocketRequesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian935.retrosocketrequester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrosocketRequesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
